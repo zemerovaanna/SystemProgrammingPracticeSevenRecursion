ddfb9e24465abaeddb72b4bc18d0a3888d13524c
Anna <1zemerovaanna@gmail.com>
2024-02-11 12:05:53 +0500

    Update

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int Сount(int n, int u, int count)
+{
+    if (n > u)
+    {
+        n = n - 1;
+        u = u + 1;
+        count = count + 1;
+        return Сount(n, u, count);
+    }
+    else
+    {
+        return count;
+    }
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int n;
+	cout << "Пожалуйста, натуральное число: ";
+	cin >> n;
+	cout << "Результат: " << Сount(n - 1, 1, 0) << endl;
+	return 0;
+}
